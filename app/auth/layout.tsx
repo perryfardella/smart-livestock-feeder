475ac5f563c6d88d16e0423514868c9c846391ac
Perry Fardella <perry.fardella@live.com>
2025-05-22 14:06:00 +0700

    Clean up mobile padding on auth pages

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex-1 flex items-center justify-center py-12">
+    <div className="flex-1 flex items-center justify-center py-12 px-4">
       <div className="w-full max-w-md space-y-8">{children}</div>
     </div>
   );
